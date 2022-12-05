e71fad7987ac30156b66e8f8c2accfd172d7dc32
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-12-04 21:50:40 -0300

    Funcion tiempo ahora anda en el futuro

--- src/app/core/helpers/time.ts
+++ src/app/core/helpers/time.ts
@@ -1,39 +1,40 @@
-export function howLongFromPastDate(pastDate: Date | string,short=false):string{
+export function howLongFromPastDate(futureDate: Date | string,short=false):string{
   let date: Date;
-  if(typeof pastDate === "string"){
-    date = new Date(pastDate)
+  if(typeof futureDate === "string"){
+    date = new Date(futureDate)
   } else {
-    date = pastDate
+    date = futureDate
   }
-  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+  const seconds = Math.floor((date.getTime() - new Date().getTime()) / 1000);
   let intervalo = seconds / 31536000;
   if (intervalo > 1) {
-    if (intervalo < 2) return !short ? "Hace un año" : "1 y";
-    return "Hace " + Math.floor(intervalo) + (!short ? " años" : " y");
+    if (intervalo < 2) return !short ? "Dentro de un año" : "1 y";
+    return "Dentro de " + Math.floor(intervalo) + (!short ? " años" : " y");
   }
 
   intervalo = seconds / 2592000;
   if (intervalo > 1) {
-    if (intervalo < 2) return !short ? "Hace un año" : "1 m";
-    return "Hace " + Math.floor(intervalo) + (!short ? " meses" : " m");
+    if (intervalo < 2) return !short ? "Dentro de un año" : "1 m";
+    return "Dentro de " + Math.floor(intervalo) + (!short ? " meses" : " m");
   }
 
   intervalo = seconds / 86400;
   if (intervalo > 1) {
-    if (intervalo < 2) return !short ? "Ayer" : "1 d";
-    return "Hace " + Math.floor(intervalo) + (!short ? " días" : " d");
+    if (intervalo < 2) return !short ? "Mañana" : "1 d";
+    return "Dentro de " + Math.floor(intervalo) + (!short ? " días" : " d");
   }
 
   intervalo = seconds / 3600;
   if (intervalo > 1) {
-    if (intervalo < 2) return !short ? "Hace una hora" : "1 h";
-    return "Hace " + Math.floor(intervalo) + (!short ? " horas" : " h");
+    if (intervalo < 2) return !short ? "Dentro de una hora" : "1 h";
+    return "Dentro de " + Math.floor(intervalo) + (!short ? " horas" : " h");
   }
 
   intervalo = seconds / 60;
   /*if (intervalo > 1) {
-    if (intervalo < 2) return !short ? "Hace 1 minuto" : "1 m";
-    return "Hace " + Math.floor(intervalo) + (!short ? " minutos" : " mm");
+    if (intervalo < 2) return !short ? "Dentro de 1 minuto" : "1 m";
+    return "Dentro de " + Math.floor(intervalo) + (!short ? " minutos" : " mm");
   }*/
-  return (!short ? "Ahora" : "Ahora");
+
+  return (!short ? "Pasado" : "Pasado");
 }
